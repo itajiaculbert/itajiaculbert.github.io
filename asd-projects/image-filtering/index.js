ff9d028afa9ab1c5a735d60aaf974ccdfe5f9234
itajia culbert <itajiaculbert16@gmail.com>
2024-04-09 13:33:08 +0000

    saving image filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,8 +20,9 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
-
-  
+  applyFilterNoBackground(reddify);
+  applyFilterNoBackground(increaseGreenByBlue);
+  applyFilter(decreaseBlue);
 
   // do not change the below line of code
   render($("#display"), image);
@@ -32,18 +33,52 @@ function applyAndRender() {
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2 & 4: Create the applyFilter function here
-
+function applyFilter(filterFunction){
+  for(var r = 0; r < image.length; r++){
+    for(var c = 0; c < image[r].length; c++){
+      var rgbString = image[r][c];
+      var rgbNumbers = rgbStringToArray(rgbString);
+      filterFunction(rgbNumbers);
+      rgbString = rgbArrayToString(rgbNumbers);
+      image[r][c] = rgbString;
+    }
+  }
+}
 
 // TODO 7: Create the applyFilterNoBackground function
-
+function applyFilterNoBackground(filterFunction){
+  var backgroundColor = image[0][0];
+  for(var r = 0; r < image.length; r++){
+    for(var c = 0; c < image[r].length; c++){
+      var rgbString = image[r][c];
+      if(rgbString !== backgroundColor){
+      var rgbNumbers = rgbStringToArray(rgbString);
+      filterFunction(rgbNumbers);
+      rgbString = rgbArrayToString(rgbNumbers);
+      image[r][c] = rgbString;
+      }
+    }
+  }
+}
 
 // TODO 5: Create the keepInBounds function
-
+function keepInBounds(num){
+   num = num < 0 ? 0 : num;
+   num = num > 255 ? 255 : num;
+   return num;
+}
 
 // TODO 3: Create reddify function
-
+function reddify(arr){
+  arr[RED] = 200;
+}
 
 // TODO 6: Create more filter functions
-
+function decreaseBlue(arr){
+  arr[BLUE] = keepInBounds(arr[BLUE] - 50);
+}
+function increaseGreenByBlue(arr){
+  arr[GREEN] = keepInBounds(arr[BLUE] + arr[GREEN]);
+}
 
 // CHALLENGE code goes below here
